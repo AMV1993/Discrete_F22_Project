983c3c7a28a782c6759837bf2203d34330480292
AMV1993 <anva1564@colorado.edu>
2022-12-05 16:46:02 -0700

    working on formating/general concepts behind program

--- RSA_main.py
+++ RSA_main.py
@@ -1,4 +1,4 @@
-#####################################################
+##################################################################
 #Author: Anton Vandenberge
 #Date 12/5/2022
 #Description:
@@ -7,5 +7,18 @@
 #   and understand the process of creating the Key values
 #   and how to decrypt an encrypted message
 #Bonus: 
-#####################################################
+##################################################################
 
+#Functions#
+
+
+#maincode#
+
+""" Wnat the user to be able to click enter to step through and see how the math works for doing an RSA encryption 
+    First i need to employ a simple RSA encryption to get full credit for the final project"""
+
+# Step 1 RSA Key Generation
+# the "sender" selects two prime numbers p and q and an exponent e
+# p, q, and e are the private key for the sender
+cstr = "Step 1 for RSA Encryption"
+print(cstr.center(40, '-'))
