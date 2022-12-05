01a9c4f17c5a7d8cef27a4bc9528d6dd9b872d8a
AMV1993 <anva1564@colorado.edu>
2022-12-05 02:27:52 -0700

    adding header block

---
+++ RSA_main.py
@@ -0,0 +1,11 @@
+#####################################################
+#Author: Anton Vandenberge
+#Date 12/5/2022
+#Description:
+#   Final discrete mathematics project, Implementing
+#   an RSA encryption method allowing the user to step through
+#   and understand the process of creating the Key values
+#   and how to decrypt an encrypted message
+#Bonus: 
+#####################################################
+
