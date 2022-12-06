60d7c996c2f9db7a021e385eddcbb07dbfa72dbd
AMV1993 <anva1564@colorado.edu>
2022-12-05 19:27:17 -0700

    finally got the gcd correct

--- RSA_main.py
+++ RSA_main.py
@@ -10,15 +10,25 @@
 ##################################################################
 #Imports#
 import math
+from fractions import gcd
 
 
 #Functions#
 # calculate_e function takes in the user inputs for p and q and evaluates numbers for
 # the exponent e that are relatively prime to (q-1)(p-1)
 def calculate_e(x):
-
-
+    for a in range(10000,100000,1110):
+        if math.gcd(x,a) == 1:
+            print(a, end=" , ")
     return
+
+def calculate_gcd(x,y):
+    a = 0
+    if math.gcd(x,y) == 1:
+        a = 1
+    return a
+    
+
 
 #maincode#
 
@@ -40,13 +50,33 @@ x = (p-1)*(q-1)
 
 print("Based on your p and q values of ",p, " and ", q, "\nyou need to select a value for e that is relativley prime to","\nthe number (q-1)(p-1) which is: ",x)
 
-yn = input("Would you like to see some values of e that you can use? eneter Y for yes or N for no if you have your own e value in mind \nY for yes, N for no : ")
+#yn = input("Would you like to see some values of e that you can use? eneter Y for yes or N for no if you have your own e value in mind \nY for yes, N for no : ")
+#YN = yn.upper()
 
-if ((yn == 'Y') or (yn == 'y')):
-    calculate_e(x)
-elif((yn == 'N')or(yn == 'n')):
-    e = input("what would you like your e value to be: ")
+#print (YN)
+# while ((yn.upper() != 'Y')  or (yn.upper() != 'N')):
+#     print("ooops please enter in either Y for yes or N for no")
+#     yn = input("Would you like to see some values of e that you can use? eneter Y for yes or N for no if you have your own e value in mind \nY for yes, N for no : ")
+#     if (yn == 'Y') or (yn == 'N'):
+#         break
 
+     
+# if (yn == 'Y'):
+#     calculate_e(x)
+# elif((yn == 'N')or(yn == 'n')):
+#     e = input("what would you like your e value to be: ")
+
+
+while True:
+    e = int(input("what would you like your exponent value e to be? :"))
+    checkNum = calculate_gcd(e,x)
+    #print(checkNum)
+    if checkNum == 1:
+        break
+    else:
+        print("That number is not relatively prime to ",x," please try again")
+
+print("Now that you have your")
 
 
 
